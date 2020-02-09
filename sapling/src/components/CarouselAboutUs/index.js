ec3ffdbc9a1a04fa31ea85beef4ce102cd1258f0
Casey Moldavon <cmoldavon@gmail.com>
2020-02-09 11:33:46 -0800

    Adding carousel functionality and Updating files/folders

---
+++ sapling/src/components/CarouselAboutUs/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import "./style.css";
+
+
+// This carousel will auto-change over time through images
+
+function CarouselAboutUs() {
+    return (
+    <Carousel>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="../images/amazon-dog-3.gif"
+            alt="First slide"
+            />
+
+            <Carousel.Caption>
+            <h3>First slide</h3>
+            <p>First slide description goes here.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="../images/target-symbol.gif"
+            alt="Third slide"
+            />
+
+            <Carousel.Caption>
+            <h3>Second slide</h3>
+            <p>Second slide description goes here.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="../images/macys-logo.gif"
+            alt="Third slide"
+            />
+
+            <Carousel.Caption>
+            <h3>Third slide</h3>
+            <p>Third slide description goes here.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
+    )
+}
+
+
+export default CarouselAboutUs;
---
+++ sapling/src/components/CarouselSuggestions/index.js
@@ -0,0 +1,4 @@
+// this carousel appears on the product page (suggestions)
+// gives related products to user
+// user will be able to button through options
+// Cards will go here (Racheal is making cards)
