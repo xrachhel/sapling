73331c0e6ffe882312087c07a27330ff52230c5c
Rachel Yeung <rachel.cmyeung@gmail.com>
2020-02-10 14:24:27 -0800

    rachel added

---
+++ sapling/src/components/Columns/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Col from 'react-bootstrap/Col';
+
+function Column(props){
+    return(
+        <Col xs={6} md={3}></Col>
+    )
+}
+
+
+
+export default Column;
