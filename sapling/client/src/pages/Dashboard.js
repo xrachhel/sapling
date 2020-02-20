73f856dd289fd7e8179090fe1c5734d395907963
Rachel Yeung <rachel.cmyeung@gmail.com>
2020-02-20 15:45:33 -0800

    added modals to dashboard and functionality to show only one modal at a time

--- sapling/client/src/pages/Dashboard.js
+++ sapling/client/src/pages/Dashboard.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar, Nav, Button, Container, FormControl, Col, CardColumns } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import Spinner from 'react-bootstrap/Spinner';
 import Card from 'react-bootstrap/Card';
 import { useStoreContext } from "../utils/GlobalState";
@@ -11,6 +12,13 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
 
     const [state, dispatch] = useStoreContext();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+
+    const getModal = value => {
+        setShow(value)
+    };
 
     useEffect(() => {
         getTrackedItems();
@@ -18,61 +26,71 @@ const Dashboard = () => {
 
     const getTrackedItems = () => {
         API.getOneUser("5e4efb9517fcafdbdb20302c")
-        .then(res => {
-            console.log(res.data.trackedProducts)
-            dispatch({type:SET_DASHBOARD_LIST, trackedList: res.data.trackedProducts})
-        })
-        .then(res => {
-            console.log(state.trackedList)
-        })
-        .catch( err => console.log(err))
+            .then(res => {
+                console.log(res.data.trackedProducts)
+                dispatch({ type: SET_DASHBOARD_LIST, trackedList: res.data.trackedProducts })
+            })
+            .catch(err => console.log(err))
     };
 
 
     return (
         <div>
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Sapling</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Navbar>
+                <Navbar.Brand href="#home">Sapling</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
 
-        <Container>
-             <h1>Products:</h1>
-                    <Col className="md-4">
-                        {!state.trackedList.length ? (
-                            <h1>No products to display</h1>
-                        ) : (<Container>
-                            <CardColumns>
-                                {state.trackedList.map(product => {
-                                    return (
-                                        <Card key={product.name} >
-                                            <Card.Img variant="top" src={product.image} style={{ width: "45%" }} className="ml-5 pl-5 pt-5" />
-                                            <Card.Body className="text-center">
-                                                <Card.Title>{product.name}</Card.Title>
-        
-                                            </Card.Body>
-                                        </Card>
-                                    );
-                                })}
-                            </CardColumns>
-                        </Container>
-                            )}
-                    </Col>
-        </Container>
+            <Container>
+                <h1>Your Tracked Products:</h1>
+                <Col className="md-4">
+                    {!state.trackedList.length ? (
+                        <h1>No products to display</h1>
+                    ) : (<Container>
+                        <CardColumns>
+                            {state.trackedList.map((product, index) => (
+                                <div>
+                                    <Card key={product.name} >
+                                        <Card.Img variant="top" src={product.image} style={{ width: "45%" }} className="ml-5 pl-5 pt-5" />
+                                        <Card.Body className="text-center">
+                                            <Card.Title>{product.name}</Card.Title>
+                                            <Button variant="primary" onClick={() => getModal(index)}>See Product Info</Button>
+                                        </Card.Body>
+                                    </Card>
+                                    <Modal show={show === index} onHide={handleClose}>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Modal heading</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>{product.name}</Modal.Body>
+                                        <Modal.Footer>
+                                            <Button variant="secondary" onClick={handleClose}>
+                                                Close
+                                                </Button>
+                                            <Button variant="primary" onClick={handleClose}>
+                                                Save Changes
+                                                </Button>
+                                        </Modal.Footer>
+                                    </Modal>
 
+                                </div>
+                            ))}
+                        </CardColumns>
 
+                    </Container>
 
-
+                        )}
+                </Col>
+            </Container>
 
 
 
